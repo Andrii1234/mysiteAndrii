3ae8e0a44f4430210e01d502f47e757e79390ebf
Andrii Kolosov <andriikolosov@MacBook-Pro-Andrii.local>
2019-08-15 12:54:50 +0300

    Andrii

---
+++ mainApp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'mainApp/homePage.html')
+
+
+def  contact(request):
+    return render(request, 'mainApp/basic.html', {'contact': ['если остальсь вопросы пишите нам','38003']})
