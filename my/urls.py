f1f3b753f962804bca9f59279ee47a4f10f48466
Andrii Kolosov <andriikolosov@MacBook-Pro-Andrii.local>
2019-08-16 12:50:29 +0300

    2

--- my/urls.py
+++ my/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainApp.urls')),
+    path('news/', include('news.urls')),
 
 
 ]
---
+++ news/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from django .views.generic import ListView, DetailView
+from news.models import Articles
+
+urlpatterns = [
+    url('',
+    ListView.as_view(queryset = Articles.objects.all().order_by("-date")[:20],
+    template_name = "news/post.html")),
+]
